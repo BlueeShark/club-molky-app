69010dd51b1d1077f5aa8aeaa28a31564f516adf
Adrien Macaire <admcr019@gmail.com>
2025-09-23 11:55:39 +0200

    Règlement du footer

--- src/component/templates/Navbar.tsx
+++ src/component/templates/Navbar.tsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export function Navbar() {
   return (
     <div>
-      <Link to={"/"}>Acceuil</Link>
+      <Link to={"/"}>Accueil</Link>
       <Link to={"/news"}>Actualité</Link>
+      <Link to={"/event"}> Evènements</Link>
+      <Link to={"/about"}>A propos</Link>
     </div>
   );
 }
